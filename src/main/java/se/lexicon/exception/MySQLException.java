2b8d2053ad094bc49faa1bc3905c5f58384fe2b6
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-09-05 15:41:15 +0200

    modified

--- src/main/java/se/lexicon/exception/MySQLException.java
+++ src/main/java/se/lexicon/exception/MySQLException.java
@@ -1,4 +1,12 @@
 package se.lexicon.exception;
 
-public class MySQLException {
+public class MySQLException extends RuntimeException {
+
+    public MySQLException(String message) {
+        super(message);
+    }
+
+    public MySQLException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
