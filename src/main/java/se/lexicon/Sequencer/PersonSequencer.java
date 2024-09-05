c0874671354d8a76d744eb305509673bf2d943ac
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-09-05 13:02:42 +0200

    Implemented

--- src/main/java/se/lexicon/Sequencer/PersonSequencer.java
+++ src/main/java/se/lexicon/Sequencer/PersonSequencer.java
@@ -1,4 +1,10 @@
 package se.lexicon.Sequencer;
 
 public class PersonSequencer {
+
+    private static int sequencer = 0;
+
+    public static int nextId() {
+        return ++sequencer;
+    }
 }
--- src/main/java/se/lexicon/Sequencer/TodoItemSequencer.java
+++ src/main/java/se/lexicon/Sequencer/TodoItemSequencer.java
@@ -1,4 +1,10 @@
 package se.lexicon.Sequencer;
 
 public class TodoItemSequencer {
+
+    private static int sequencer = 0;
+
+    public static int nextId() {
+        return ++sequencer;
+    }
 }
