3f53beafa5c5a69f9c0c7162a14f33b4cece0cad
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-09-05 16:05:42 +0200

    modified

--- src/main/java/se/lexicon/Dao/PersonDao.java
+++ src/main/java/se/lexicon/Dao/PersonDao.java
@@ -3,12 +3,13 @@ package se.lexicon.Dao;
 import se.lexicon.model.Person;
 
 import java.util.Collection;
+import java.util.Optional;
 
 public interface PersonDao {
 
     Person create(Person person);
     Collection<Person> findAll();
-    Person findById();
+    Optional<Person> findById();
     Collection<Person> findByName();
     Person update();
     Boolean deleteById();
