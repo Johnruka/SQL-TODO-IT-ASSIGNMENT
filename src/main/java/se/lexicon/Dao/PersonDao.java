4c6699b9be4ae95d21631c082b152df121caa7fa
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-09-05 13:07:59 +0200

    Implemented

--- src/main/java/se/lexicon/Dao/PersonDao.java
+++ src/main/java/se/lexicon/Dao/PersonDao.java
@@ -1,4 +1,15 @@
 package se.lexicon.Dao;
 
+import se.lexicon.model.Person;
+
+import java.util.Collection;
+
 public interface PersonDao {
+
+    Person create(Person person);
+    Collection<Person> findAll();
+    Person findById();
+    Collection<Person> findByName();
+    Person update();
+    Boolean deleteById();
 }
--- src/main/java/se/lexicon/Dao/TodoItemDao.java
+++ src/main/java/se/lexicon/Dao/TodoItemDao.java
@@ -1,4 +1,17 @@
 package se.lexicon.Dao;
 
+import com.sun.tools.javac.comp.Todo;
+
+import java.util.Collection;
+
 public interface TodoItemDao {
+
+    Todo create();
+    Collection<Todo> findAll();
+    Todo findById();
+    Collection<Todo>findByDoneStatus();
+    Collection<Todo>findByAssignee();
+    Collection<Todo> findByUnassignedTodoItems();
+    Todo update();
+    boolean deleteById();
 }
